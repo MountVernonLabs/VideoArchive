0b6c62e58c124637db223b35964b4d2b9fdebaa5
Matt Briney <mbriney@Matts-13-MacBook-Pro.local>
2016-12-05 00:52:35 -0500

    Initial commit
    Just starting the project, lots to still do.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # VideoArchive
-Creates a web-based tool to organize video files into an archive with tagging and contact sheet functionality.  Storage of video files in Amazon Glacier.
+Creates a web-based tool to organize video files into an archive with tagging and contact sheet functionality.  Storage of video files in Amazon Cloud Drive.  Seeking framework for web display, may rely on BigTreeCMS.
+
+# To Do
+- Create uploader for Amazon Cloud Drive based on acd_cli
+- Sync script to populate MySQL database table
+
+# Dependancies
+- FFMpeg
+- ImageMagick
+- acd_cli
+- MySQL
+- PHP
+
+# Thanks To
+@melmatsuoka for the video contact sheet script
+https://gist.github.com/melmatsuoka/b87c4dc9374c52827582
+
+@yadayada for acd_cli
+https://github.com/yadayada/acd_cli
---
+++ videoContactSheet.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# Generates thumbnail contact sheets of all video files in current working directory.
+#
+# Script defaults to writing PNG contact sheets to the same folder, using the original
+# video filename as the basename for the contact sheet
+#
+# More details: https://trac.ffmpeg.org/wiki/Create%20a%20thumbnail%20image%20every%20X%20seconds%20of%20the%20video
+#
+# NOTE: 'montage' requires that Ghostscript be installed, in order to be able to generate titles
+# on the contact sheet: run 'brew install ghostscript' to install
+
+
+function cleanup () {
+
+	rm -rf .snapshots/*
+}
+
+SNAPSHOT_INTERVAL="15" # time interval (in seconds) to grab each frame
+
+
+if [ ! -d ".snapshots" ] # check for existence of screengrab temp folder in CWD
+  then mkdir .snapshots
+fi
+
+shopt -s nocaseglob 	# forces case insensitive extension globbing in the following for loop
+
+for FILE in *.{wmv,avi,rm,ram,mpg,mpeg,mov,mp4,flv,asf,mkv,m4v};
+
+	do [ -e "$FILE" ] || continue;
+
+		trap cleanup SIGHUP SIGINT SIGTERM
+		# extract thumbnail frames every $SNAPSHOT_INTERVAL seconds. By telling FFmpeg to set the output files FPS option to
+		# a very low value, we made FFmpeg drop a lot of frames at the output, in order to achieve such a low frame
+		# rate, effectively having our thumbnails generated every X seconds
+		#
+		# thumbnails are written to a hidden .snapshots temp folder in the same folder the script was run in.
+
+		echo "------------------------------------------------"
+		echo -e "\nExtracting thumbnails for \"${FILE}\""
+		ffmpeg -loglevel warning -i "${FILE}" -f image2 -vf fps=fps=1/$SNAPSHOT_INTERVAL .snapshots/._"${FILE}"_%03d.png 2> /dev/null
+
+		# assemble the contact sheet, using ImageMagick's "montage" util
+
+		echo -e "Compiling contact sheet for \"${FILE}\""
+		montage .snapshots/"._${FILE}"_*.png -geometry +3+1 -title "${FILE}" "${FILE}.png"
+
+		# purge the temporary .snapshots folder
+
+		echo -e "Cleaning up tempfiles...\n"
+		rm -f .snapshots/._"${FILE}"_*.png
+
+	done
